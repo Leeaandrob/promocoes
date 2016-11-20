4bd52e44cbbaef7c5187169e5c106d132fb568e6
Leandro Barbosa <leandrobar93@gmail.com>
2016-11-20 15:11:44 -0200

    add app core and change main url to homeview

--- promocoes/urls.py
+++ promocoes/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from core.views import HomeView
+
 
 urlpatterns = [
+    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
 ]
